cf9e870485080afb02238d1cbb14784c6c520736
NataliaNikitina <nne2418@gmail.com>
2023-08-30 22:03:47 +0300

    Initial Commit

---
+++ ARKitHouse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ARKitHouse
+//
+//  Created by Natalia N on 30.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
